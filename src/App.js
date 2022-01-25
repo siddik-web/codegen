5f3777de2ad36d959299aa0d08c18cfe3eb96ecf
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-01-26 01:36:23 +0600

    add all field into state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     author: "",
     creationDate: "",
     copyright: "",
+    license:"",
     authorEmail: "",
     authorUrl: "",
     version: "",
@@ -16,31 +17,62 @@ function App() {
   });
   const handelNameChange = (e) => {
     localStorage.setItem(
-      "manifest_key",
-      JSON.stringify({ ...manifest, name: e.target.value })
+      "manifest_key", JSON.stringify({ ...manifest, name: e.target.value })
     );
     setManifest({ ...manifest, name: e.target.value });
   };
   const handelAuthorChange = (e) => {
     localStorage.setItem(
-      "manifest_key",
-      JSON.stringify({ ...manifest, author: e.target.value })
+      "manifest_key", JSON.stringify({ ...manifest, author: e.target.value })
     );
     setManifest({ ...manifest, author: e.target.value });
   };
   const handelCreationDateChange = (e) => {
     localStorage.setItem(
-      "manifest_key",
-      JSON.stringify({ ...manifest, creationDate: e.target.value })
+      "manifest_key", JSON.stringify({ ...manifest, creationDate: e.target.value })
     );
     setManifest({ ...manifest, creationDate: e.target.value });
   };
 
+  const handelCopyrightChange = (e) => {
+    localStorage.setItem(
+      "manifest_key", JSON.stringify({ ...manifest, copyright: e.target.value })
+    );
+    setManifest({ ...manifest, copyright: e.target.value });
+  };
+
+  const handelAuthorEmailChange = (e) => {
+    localStorage.setItem(
+      "manifest_key", JSON.stringify({ ...manifest, authorEmail: e.target.value })
+    );
+    setManifest({ ...manifest, authorEmail: e.target.value });
+  };
+
+  const handelAuthorUrlChange = (e) => {
+    localStorage.setItem(
+      "manifest_key", JSON.stringify({ ...manifest, authorUrl: e.target.value })
+    );
+    setManifest({ ...manifest, authorUrl: e.target.value });
+  };
+
+  const handelVersionChange = (e) => {
+    localStorage.setItem(
+      "manifest_key", JSON.stringify({ ...manifest, version: e.target.value })
+    );
+    setManifest({ ...manifest, version: e.target.value });
+  };
+
+  const handelDescriptionChange = (e) => {
+    localStorage.setItem(
+      "manifest_key", JSON.stringify({ ...manifest, description: e.target.value })
+    );
+    setManifest({ ...manifest, description: e.target.value });
+  };
+
   useEffect(() => {
     if (localStorage.getItem("manifest_key") === null) {
-      setManifest(
-        localStorage.setItem("manifest_key", JSON.stringify(manifest))
-      );
+      setManifest(JSON.parse(localStorage.getItem("manifest_key")));
+      localStorage.setItem("manifest_key", JSON.stringify(manifest))
     } 
   }, [manifest]);
   console.log(manifest);
@@ -72,40 +104,46 @@ function App() {
               id={"copyright"}
               label={"copyright"}
               type={"text"}
-              onChange={handelAuthorChange}
+              onChange={handelCopyrightChange}
             />
             <InputBoxWithLabel
               id={"authorEmail"}
               label={"author Email"}
               type={"text"}
-              onChange={handelAuthorChange}
+              onChange={handelAuthorEmailChange}
             />
             <InputBoxWithLabel
               id={"authorUrl"}
               label={"authorUrl"}
               type={"text"}
-              onChange={handelAuthorChange}
+              onChange={handelAuthorUrlChange}
             />
             <InputBoxWithLabel
               id={"version"}
               label={"version"}
               type={"text"}
-              onChange={handelAuthorChange}
+              onChange={handelVersionChange}
             />
             <InputBoxWithLabel
               id={"description"}
               label={"description"}
               type={"text"}
-              onChange={handelAuthorChange}
+              onChange={handelDescriptionChange}
             />
           </div>
           <div className="col border p-3">
             <CodeBlock
               type={"module"}
               client={"site"}
-              name={manifest?.name}
-              author={manifest?.author}
-              creationDate={manifest?.creationDate}
+              name={JSON.parse(localStorage.getItem('manifest_key'))?.name}
+              author={JSON.parse(localStorage.getItem('manifest_key'))?.author}
+              creationDate={JSON.parse(localStorage.getItem('manifest_key'))?.creationDate}
+              copyright={JSON.parse(localStorage.getItem('manifest_key'))?.copyright}
+              license={JSON.parse(localStorage.getItem('manifest_key'))?.license}
+              authorEmail={JSON.parse(localStorage.getItem('manifest_key'))?.authorEmail}
+              authorUrl={JSON.parse(localStorage.getItem('manifest_key'))?.authorUrl}
+              version={JSON.parse(localStorage.getItem('manifest_key'))?.version}
+              description={JSON.parse(localStorage.getItem('manifest_key'))?.description}
             />
           </div>
         </div>
--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-export default function CodeBlock({ type, client, group, name, author, creationDate }) {
+export default function CodeBlock({
+  type,
+  client,
+  group,
+  name,
+  author,
+  creationDate,
+  copyright,
+  license,
+  authorEmail,
+  authorUrl,
+  version,
+  description,
+}) {
   return (
     <>
       <pre>
         <code>
-          {`<xml version="1.0" encoding="utf-8"/>\n<extension type="${type}" version="3.9" client="${client}" group="${group}" method="upgrade">\n\t<name>${name}</name>\n\t<author>${author}</author>\n\t<creationDate>${creationDate}</creationDate>\n</extension>`}
+          {`<xml version="1.0" encoding="utf-8"/>\n<extension type="${type}" version="3.9" client="${client}" group="${group}" method="upgrade">\n\t<name>${name}</name>\n\t<author>${author}</author>\n\t<creationDate>${creationDate}</creationDate>\n\t<copyright>${copyright}</copyright>\n\t<license>${license}</license>\n\t<authorEmail>${authorEmail}</authorEmail>\n\t<authorUrl>${authorUrl}</authorUrl>\n\t<version>${version}</version>\n\t<description>${description}</description>\n</extension>`}
         </code>
       </pre>
     </>
