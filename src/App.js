d1fba35b732acd5c0c956543f725e22fbc598d0e
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-01-26 00:54:10 +0600

    add local storage and setState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,113 @@
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import './App.css';
-import List from './components/inputs/List';
-import Select from './components/inputs/Select';
+import { useEffect, useState } from "react";
+import "./App.css";
+import CodeBlock from "./components/CodeBlock";
+import InputBoxWithLabel from "./components/inputs/InputBoxWithLabel";
 
 function App() {
-  const data = [1,2,3,4];
+  const [manifest, setManifest] = useState({
+    name: "",
+    author: "",
+    creationDate: "",
+    copyright: "",
+    authorEmail: "",
+    authorUrl: "",
+    version: "",
+    description: "",
+  });
+  const handelNameChange = (e) => {
+    localStorage.setItem(
+      "manifest_key",
+      JSON.stringify({ ...manifest, name: e.target.value })
+    );
+    setManifest({ ...manifest, name: e.target.value });
+  };
+  const handelAuthorChange = (e) => {
+    localStorage.setItem(
+      "manifest_key",
+      JSON.stringify({ ...manifest, author: e.target.value })
+    );
+    setManifest({ ...manifest, author: e.target.value });
+  };
+  const handelCreationDateChange = (e) => {
+    localStorage.setItem(
+      "manifest_key",
+      JSON.stringify({ ...manifest, creationDate: e.target.value })
+    );
+    setManifest({ ...manifest, creationDate: e.target.value });
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("manifest_key") === null) {
+      setManifest(
+        localStorage.setItem("manifest_key", JSON.stringify(manifest))
+      );
+    } 
+  }, [manifest]);
+  console.log(manifest);
   return (
     <div>
       <h1 className="text-center">CodeGen</h1>
       <div className="container">
         <div className="row">
           <div className="col border p-3">
-            <Select items={data}></Select>
+            <InputBoxWithLabel
+              id={"name"}
+              label={"name"}
+              type={"text"}
+              onChange={handelNameChange}
+            />
+            <InputBoxWithLabel
+              id={"author"}
+              label={"author"}
+              type={"text"}
+              onChange={handelAuthorChange}
+            />
+            <InputBoxWithLabel
+              id={"creationDate"}
+              label={"Creation Date"}
+              type={"text"}
+              onChange={handelCreationDateChange}
+            />
+            <InputBoxWithLabel
+              id={"copyright"}
+              label={"copyright"}
+              type={"text"}
+              onChange={handelAuthorChange}
+            />
+            <InputBoxWithLabel
+              id={"authorEmail"}
+              label={"author Email"}
+              type={"text"}
+              onChange={handelAuthorChange}
+            />
+            <InputBoxWithLabel
+              id={"authorUrl"}
+              label={"authorUrl"}
+              type={"text"}
+              onChange={handelAuthorChange}
+            />
+            <InputBoxWithLabel
+              id={"version"}
+              label={"version"}
+              type={"text"}
+              onChange={handelAuthorChange}
+            />
+            <InputBoxWithLabel
+              id={"description"}
+              label={"description"}
+              type={"text"}
+              onChange={handelAuthorChange}
+            />
           </div>
           <div className="col border p-3">
-           <Select items={data}></Select>
-           </div>
-           <DndProvider backend={HTML5Backend}>
-          <div className="col border p-3">
-            <List items={data}/>
+            <CodeBlock
+              type={"module"}
+              client={"site"}
+              name={manifest?.name}
+              author={manifest?.author}
+              creationDate={manifest?.creationDate}
+            />
           </div>
-          <div className="col border p-3">
-
-          </div>
-          </DndProvider>
         </div>
       </div>
     </div>
--- src/redux/contants/actionsTypes.js
+++ src/redux/contants/actionsTypes.js
@@ -1,7 +1,8 @@
 const types = {
     ADD_ITEM: "ADD_ITEM",
     EDIT_ITEM: "EDIT_ITEM",
-    DELETE_ITEM: "DELETE_ITEM"
+    DELETE_ITEM: "DELETE_ITEM",
+    ADD_MANIFEST: 'ADD_MANIFEST'
 }
 
 export default types;
